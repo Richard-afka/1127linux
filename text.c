3f6939792dd1be7057a0c824e8df8de418a95047
zhou <1276540797@qq.com>
2025-12-04 22:40:24 +0800

    -

--- text.c
+++ text.c
@@ -1,5 +1,13 @@
-#include<stdio.h>
+
 int main(){
     printf("11\n");
     return 0;
+}
+#include <stdio.h>
+int main(){
+	int a,b;
+	scanf("%d %d",&a,&b);
+	int sum=a+b;
+	printf("%d",sum);
+	return 0;
 }
