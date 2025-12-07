7432d924787e01a106521b6668ec8dec93215b77
lfd <2281409362@qq.com>
2025-12-07 22:20:28 +0800

    text.c

--- text.c
+++ text.c
@@ -3,6 +3,10 @@ int add(int a, int b);
 int subtract(int a, int b);
 int multiply(int a, int b);
 int divide(int a, int b);
+char *  hello()
+{
+    return "hello";
+}
 int main() {
     int a, b;
     scanf("%d %d", &a, &b);
